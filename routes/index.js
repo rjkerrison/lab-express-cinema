cb4f8c506f62e2a24b951c4982d6f88b450f32c0
Adrien Deveaux <adrien.dvx@gmail.com>
2022-06-03 17:53:25 +0200

    done without bonus for now (wink wink Haroun

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,30 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.model.js");
 
 /* GET /
 
 This is a health check. It allows us to see that the API is running.
 */
-router.get('/', (req, res, next) =>
-  res.json({ success: true, name: 'lab-express-cinema' })
-)
+router.get("/", async (req, res, next) => {
+  try {
+    const allMovies = await Movie.find().select({ title: 1, director: 1 });
+    res.json(allMovies) 
+    // { success: true, name: "lab-express-cinema" });
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+router.get("/:id", async (req, res, next) => {
+  // Iteration #4: Update the drone
+  try {
+    const id = req.params.id;
+    const movieInfo = await Movie.findById(id)
+    res.status(200).json(movieInfo);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
