4a381d01488a8c9656227be6bf1acd33bfb1d630
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-05 15:45:20 +0200

    Done without bonus

--- routes/movies.js
+++ routes/movies.js
@@ -4,15 +4,26 @@ const router = express.Router();
 //Import Movie Model
 const Movie = require("../models/movie.model");
 
-/* GET /
+// GET All movies
 
-This is a health check. It allows us to see that the API is running.
-*/
 router.get("/", async (req, res, next) => {
   try {
     const movies = await Movie.find();
     console.log(movies);
     res.status(200).json(movies);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET One movie Info with the ID
+
+router.get("/:movieId", async (req, res, next) => {
+  try {
+    const movieId = req.params.movieId;
+    const oneMovie = await Movie.findById(movieId);
+    console.log(oneMovie);
+    res.status(200).json(oneMovie);
   } catch (err) {
     next(err);
   }
