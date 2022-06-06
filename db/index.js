401e4cbd40af5ecdd85f475e71df5616e0b80712
Pierre Carette <pierre.carette78@gmail.com>
2022-06-06 22:25:06 +0200

    done

--- db/index.js
+++ db/index.js
@@ -7,11 +7,15 @@ const mongoose = require("mongoose");
 
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+  
+  const openConnection = async (x) => {
+    try{
+      const x = await mongoose.connect(MONGO_URI);
+      console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    } catch(error) { 
+    console.error("Error connecting to mongo: ", error);
+  }
+  
+}
+
+module.exports = openConnection
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Film = require("../models/movie.model")
 
 /* GET /
 
@@ -8,5 +9,23 @@ This is a health check. It allows us to see that the API is running.
 router.get('/', (req, res, next) =>
   res.json({ success: true, name: 'lab-express-cinema' })
 )
+
+router.get('/movies', async (req, res, next) => {
+  try {
+    const listOfFilms = await Film.find({ title: 1, director: 1 });
+    res.json(listOfFilms)
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.get('/movies/:id', async (req, res, next) => {
+  try{
+    const movies = await Film.findById(req.params.id, { __v: 0 });
+    return res.status(200).json(movies);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router
