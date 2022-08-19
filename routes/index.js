6806ced5e6b9cf9d26aaf8408b8fe84fdd79be5a
Chlotte <charlottealleaume99@gmail.com>
2022-08-19 15:00:05 +0200

    finished lab?

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 /* GET /
-
 This is a health check. It allows us to see that the API is running.
 */
 router.get('/', (req, res, next) =>
   res.json({ success: true, name: 'lab-express-cinema' })
-)
+	  );
 
-module.exports = router
+// GET /movies
+const Movie = require('../models/Movie.model.js');
+
+router.get('/movies', async (req, res) => {
+  const movies = await Movie.find({}).select("title director");
+  res.json({ movies });
+});
+
+// GET /movie/:id
+router.get('/movie/:id', async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+  res.json({ movie });
+});
+
+module.exports = router;
