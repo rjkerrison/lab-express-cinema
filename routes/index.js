c29d99d2ea9116579254449ca20abe4eb416c548
Marko Ivezic <markoivezic89@gmail.com>
2022-08-25 11:01:59 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 /* GET /
 
 This is a health check. It allows us to see that the API is running.
 */
-router.get('/', (req, res, next) =>
-  res.json({ success: true, name: 'lab-express-cinema' })
-)
+router.get("/movies", async (req, res) => {
+  const movies = await movies.find();
+  res.json({ movies });
+});
+router.get("/", (req, res, next) =>
+  res.json({ success: true, name: "lab-express-cinema" })
+);
 
-module.exports = router
+module.exports = router;
